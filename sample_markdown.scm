2cf35c0646c17034a0041ad92bcc56d042ce525f
Zack <zphixon@gmail.com>
2020-06-05 18:30:59 -0400

    fill out some style building

--- sample_markdown.scm
+++ sample_markdown.scm
@@ -1,13 +1,14 @@
 ; lowercase-name - builtin element with its own styling rules
 ; UppercaseName - user-defined element
 ; $serif - text choices
-; #c00c00 - color
+; #c00c00 - foreground color
+; *fff8dc - background color
 ; %32 - fill percent, or how much space to take up vertically or horizontally
 
 (page
   "Page title"
   (txt $serif #303030 $indent)
-  (QuoteBox #FFF8DC)
+  (QuoteBox *FFF8DC)
   (QuoteText $no-indent $sans #606060)
   (Footnote #757575 $sans $italic))
 
@@ -28,5 +29,5 @@
     (txt Footnote %20 "from https://www.lipsum.com/")))
 
 (txt "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account"
-   "of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.")
+     "of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.")
 
