88e104c765758eb5725f5dd1f766b1d4f36f2147
Zack H <zphixon@gmail.com>
2020-07-19 19:21:54 -0400

    refactor fontstyles

--- library/src/layout/draw.rs
+++ library/src/layout/draw.rs
@@ -9,18 +9,18 @@ pub enum FontType {
     Sans,
 }
 
-#[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
-pub enum FontStyle {
-    Bold,
-    Italic,
-    Underline,
-    Strike,
+#[derive(Debug, PartialEq, Clone, Copy, Default)]
+pub struct FontStyle {
+    pub bold: bool,
+    pub italic: bool,
+    pub underline: bool,
+    pub strike: bool,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub struct Style {
     pub font_type: FontType,
-    pub font_style: HashSet<FontStyle>,
+    pub font_style: FontStyle,
     pub background: (u8, u8, u8),
     pub foreground: (u8, u8, u8),
     pub fill: u8,
@@ -31,7 +31,7 @@ impl Style {
     fn new() -> Style {
         Style {
             font_type: FontType::Serif,
-            font_style: HashSet::new(),
+            font_style: FontStyle::default(),
             background: (0xff, 0xff, 0xff),
             foreground: (0x00, 0x00, 0x00),
             fill: 1,
@@ -43,8 +43,20 @@ impl Style {
         self.font_type = font_type;
     }
 
-    fn add_font_style(&mut self, font_style: FontStyle) {
-        self.font_style.insert(font_style);
+    fn set_bold(&mut self) {
+        self.font_style.bold = true;
+    }
+
+    fn set_italic(&mut self) {
+        self.font_style.italic = true;
+    }
+
+    fn set_underline(&mut self) {
+        self.font_style.underline = true;
+    }
+
+    fn set_strike(&mut self) {
+        self.font_style.strike = true;
     }
 
     fn set_background(&mut self, background: (u8, u8, u8)) {
@@ -70,10 +82,10 @@ fn inline_styles_to_style(styles: &[InlineStyle], page_styles: &PageStyles, styl
             InlineStyle::Mono { .. } => style.set_font_type(FontType::Mono),
             InlineStyle::Serif { .. } => style.set_font_type(FontType::Serif),
             InlineStyle::Sans { .. } => style.set_font_type(FontType::Sans),
-            InlineStyle::Bold { .. } => style.add_font_style(FontStyle::Bold),
-            InlineStyle::Italic { .. } => style.add_font_style(FontStyle::Italic),
-            InlineStyle::Underline { .. } => style.add_font_style(FontStyle::Underline),
-            InlineStyle::Strike { .. } => style.add_font_style(FontStyle::Strike),
+            InlineStyle::Bold { .. } => style.set_bold(),
+            InlineStyle::Italic { .. } => style.set_italic(),
+            InlineStyle::Underline { .. } => style.set_underline(),
+            InlineStyle::Strike { .. } => style.set_strike(),
             InlineStyle::Fg { arg, .. } => style.set_foreground(*arg),
             InlineStyle::Bg { arg, .. } => style.set_background(*arg),
             InlineStyle::Fill { arg, .. } => style.set_fill(*arg),
@@ -131,12 +143,11 @@ mod test {
             style,
             Style {
                 font_type: FontType::Mono,
-                font_style: {
-                    let mut set = HashSet::new();
-                    set.insert(FontStyle::Italic);
-                    set.insert(FontStyle::Bold);
-                    set.insert(FontStyle::Underline);
-                    set
+                font_style: FontStyle {
+                    italic: true,
+                    bold: true,
+                    underline: true,
+                    ..FontStyle::default()
                 },
                 background: (0xff, 0xff, 0xff),
                 foreground: (0x00, 0x00, 0x00),
@@ -164,7 +175,7 @@ mod test {
             style,
             Style {
                 font_type: FontType::Serif,
-                font_style: HashSet::new(),
+                font_style: FontStyle::default(),
                 background: (0xb3, 0x33, 0x33),
                 foreground: (0xf3, 0x33, 0x33),
                 fill: 1,
@@ -184,7 +195,7 @@ mod test {
             style,
             Style {
                 font_type: FontType::Mono,
-                font_style: HashSet::new(),
+                font_style: FontStyle::default(),
                 background: (0xff, 0xff, 0xff),
                 foreground: (0x00, 0x00, 0x00),
                 fill: 1,
