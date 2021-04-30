9c7c63c3a061b75852644b4cc9d28c7985ff3d8f
Zack H <zphixon@gmail.com>
2021-04-30 00:10:32 -0400

    correct text rendering

--- client/src/main.rs
+++ client/src/main.rs
@@ -10,31 +10,54 @@ use image::ImageEncoder;
 fn main() {
     let size = 50.0;
 
-    let bytes = include_bytes!("C:\\Windows\\Fonts\\NotoSans-Regular.ttf") as &[u8];
+    let bytes = include_bytes!("C:\\Windows\\Fonts\\cmunrm.ttf") as &[u8];
     let font = fontdue::Font::from_bytes(bytes, fontdue::FontSettings::default()).unwrap();
     let fonts = &[font];
 
+    // you have to use Layout to get more than one glyph
     let mut layout = fontdue::layout::Layout::new(fontdue::layout::CoordinateSystem::PositiveYUp);
-    layout.append(fonts, &fontdue::layout::TextStyle::new("bingo ", size, 0));
-    layout.append(fonts, &fontdue::layout::TextStyle::new("bongo", size, 0));
+    layout.append(
+        fonts,
+        &fontdue::layout::TextStyle::new("Judge my vow ^178", size, 0),
+    );
 
+    // collect the metrics and coverage bitmaps
     let mut metrics = Vec::new();
     let mut bitmaps = Vec::new();
 
+    // rasterize each glyph
     layout.glyphs().iter().for_each(|glyph| {
+        // TODO ligatures?
         let (metric, bitmap) = fonts[0].rasterize_config(glyph.key);
         metrics.push(metric);
         bitmaps.push(bitmap);
     });
 
+    // TODO xmin?
+    // width of the resulting image
     let width = metrics.iter().map(|m| m.advance_width).sum::<f32>().ceil() as u32;
-    let height = metrics
-        .iter()
-        .map(|m| m.height + m.ymin.abs() as usize)
-        .max()
-        .unwrap() as u32;
-    dbg!(width, height);
 
+    // baseline - the bottom of letters like A or the underside of letters like lowercase q
+    // in image space (pixels from the top)
+    let baseline = metrics
+        .iter()
+        .map(|m| m.height as i32 + m.ymin)
+        .max()
+        .unwrap();
+
+    // lowest tail - how far below the baseline the longest dropping letters like g go
+    let lowest_tail = metrics.iter().map(|m| m.ymin).min().unwrap();
+
+    // height of the image in pixels
+    let height = baseline as u32
+        + if lowest_tail.is_negative() {
+            lowest_tail.abs() as u32
+        } else {
+            0
+        }
+        + 1; // need +1 because we work with pixels instead of floats to make it easier
+
+    // clear the image with white
     let mut image = image::ImageBuffer::new(width, height);
     for i in 0..image.width() {
         for j in 0..image.height() {
@@ -42,39 +65,30 @@ fn main() {
         }
     }
 
+    // the current x coordinate in image space
     let mut x: f32 = 0.;
-    for ((metric, bitmap), glyph) in metrics.iter().zip(bitmaps.iter()).zip(layout.glyphs()) {
-        println!("draw {} at {:?} {}", glyph.key.c, metric, bitmap.len());
-        //let mut row = 0;
+    for (metric, bitmap) in metrics.iter().zip(bitmaps.iter()) {
+        // ignore glyphs with no width like spaces
         if metric.width > 0 {
-            for (y, row) in bitmap.chunks(metric.width).enumerate() {
+            // write each row, from bottom to top
+            for (row_index, row) in bitmap.chunks(metric.width).rev().enumerate() {
                 for (col, coverage) in row.iter().enumerate() {
-                    let value = 255 - coverage.clamp(&1, &255);
-                    let x = x.floor() as u32 + col as u32;
-                    let y = (y as i32 + metric.ymin.abs()) as u32;
-                    if glyph.key.c == 'n' {
-                        println!("{} {}", x, y);
-                    }
-                    image.put_pixel(x, y, image::Rgba([value, value, value, 255]))
+                    // y pixel in image space is baseline offset by ymin, offset by the row index
+                    let y = (baseline as i32 - metric.ymin) as u32 - row_index as u32;
+                    // x pixel in image space is how far along we are
+                    let x = x as u32 + col as u32;
+
+                    // value is white minus coverage (darker where more coverage)
+                    let value = 255 - coverage.clamp(&0, &255);
+                    image.put_pixel(x, y, image::Rgba([value, value, value, 255]));
                 }
             }
         }
-        //for (i, pixel) in bitmap.iter().enumerate() {
-        //    if i + 1 == metric.width {
-        //        row += 1;
-        //    }
-        //    let pixel_x = metric.xmin as u32 + (i % metric.width) as u32;
-        //    let pixel_x = pixel_x + x.floor() as u32;
-        //    let pixel_y = metric.ymin + row;
-        //    let pixel_y = pixel_y.abs() as u32;
-
-        //    println!("pp {} {} {}", row, pixel_x, pixel_y);
-
-        //    image.put_pixel(pixel_x, pixel_y, image::Rgba([255u8, 255u8, 255u8, 255u8]));
-        //}
+        // move along by glyph advance width
         x += metric.advance_width;
     }
 
+    // write the image
     let image_file = std::fs::File::create("image.png").unwrap();
     let image_writer = std::io::BufWriter::new(image_file);
     let image_encoder = image::codecs::png::PngEncoder::new(image_writer);
